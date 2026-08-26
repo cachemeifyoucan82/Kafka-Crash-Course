4aad9476ff017c9299daaefda9786dd23f452ae8
cachemeifyoucan82 <yasminebradley6@gmail.com>
2026-08-25 22:35:58 -0400

    Added optimization (try/except block) for KeyboardInterrupt

--- tracker.py
+++ tracker.py
@@ -15,7 +15,7 @@ import json
 consumer_config = {
     "bootstrap.servers": "localhost:9092",
     "group.id": "order-tracker",
-     "auto.offset.reset": "earliest" 
+    "auto.offset.reset": "earliest" 
 }
 
 consumer = Consumer(consumer_config)
@@ -28,23 +28,25 @@ print("Consumer is running and subscribed to orders topic")
 - logic tells consumer to check whether there is a new event in the topic where it is described to
 - .poll() pings kafka if there is a new event i am subscribed to
 '''
-while True:
-    msg = consumer.poll(1.0)
-    if msg is None:
-        continue
-    if msg.error():
-        print("Error: ",msg.error())
-        continue
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print("Error: ",msg.error())
+            continue
 
-#new event a consumer hasn't read yet
-    value = msg.value().decode("utf-8")
-    order = json.loads(value)
+    #new event a consumer hasn't read yet
+        value = msg.value().decode("utf-8")
+        order = json.loads(value)
+        print(f"Received Order: {order['quantity']} x {order['item']} from {order['user']}")
+except KeyboardInterrupt:
+    print("\n Stopping consumer.")
 
-    print(f"Received Order: {order['quanity']} x {order['item']} from {order['user']}")
+finally:
+    consumer.close() #make sure to close consumer connection
 
 #kafka is designed to pull any the broker if there are any new messages instead of Kafka pushes any messages
 #polling lets consumers to control how and when many times they read those events and messages from different topics good for load balancing
 #subscribing to topics means that consumer is telling kafka i want to read from this specific topic but the consumer must pull to receive any events
-
-#logic to shut down gracefullt
-producer.flush()
